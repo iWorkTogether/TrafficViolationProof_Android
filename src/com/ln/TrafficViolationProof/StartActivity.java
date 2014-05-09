4e99dac7307a9a918f48a053aa871aadf1f43554
nchome <nc28282@sohu.com>
2014-05-09 13:54:13 +0800

    p;

--- src/com/ln/TrafficViolationProof/StartActivity.java
+++ src/com/ln/TrafficViolationProof/StartActivity.java
@@ -6,9 +6,7 @@ import android.util.Log;
 
 public class StartActivity extends Activity {
     private String TAG = StartActivity.class.getSimpleName();
-    /**
-     * Called when the activity is first created.
-     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
