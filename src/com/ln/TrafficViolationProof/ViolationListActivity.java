969da9e7f140febdc53f3549b87e435a6b79ec90
ramonqlee1980 <ramonqlee1980@gmail.com>
2014-05-17 06:56:09 +0800

    增加违章展示界面
    现在为空，待完善

---
+++ src/com/ln/TrafficViolationProof/ViolationListActivity.java
@@ -0,0 +1,15 @@
+package com.ln.TrafficViolationProof;
+
+import android.app.Activity;
+import android.os.Bundle;
+/*
+ * 违章查询的结果页面
+ */
+public class ViolationListActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_violation_list_activity);
+	}
+}
