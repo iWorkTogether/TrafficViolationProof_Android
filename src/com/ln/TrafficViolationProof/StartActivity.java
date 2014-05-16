b09125eed4bb6149231db2c59ed163fb2f46591b
ramonqlee1980 <ramonqlee1980@gmail.com>
2014-05-17 06:58:39 +0800

    违章展示界面的构思

--- src/com/ln/TrafficViolationProof/StartActivity.java
+++ src/com/ln/TrafficViolationProof/StartActivity.java
@@ -118,13 +118,6 @@ public class StartActivity extends Activity implements TaskListener {
 			}
 		}
 		
-		//TODO::在新的界面中展现违章信息
-		//违章界面：参考车友会的展示界面：切换城市；查询违章处理地；地图模式查看；周边违章（其他人的违章地点，原因）
-		//每个项目：详情页；标记为已处理(将不再出现在新的查询结果中)；查看违章地点(全景图)
-		//每个违章项目的设计：尽量图形化（时间，地点，原因，处罚措施，全景图（暗含可以查看全景图））
-		//延伸：
-		//1.现有查询软件，查询后就不能再查看违章信息了：退出历史违章
-		//2.
 		if (!TextUtils.isEmpty(r)) {
 			TextView tView = (TextView) findViewById(R.id.tv_result);
 			tView.setText(r);
